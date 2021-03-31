d5d6f6734d964611092289ff8ef6ecd9d8f38656
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-03-31 08:16:54 -0400

    CS50 the paddle update (30:52): make paddles move and add scores

--- Main.gd
+++ Main.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+export var paddle_speed = 800
+
 func _process(delta):
 	if (Input.is_action_pressed("ui_cancel")):
 		get_tree().quit()
+
+	if (Input.is_action_pressed("ui_down_01")):
+		$LeftPaddle.move_and_collide(Vector2(0, paddle_speed * delta))
+	elif (Input.is_action_pressed("ui_up_01")):
+		$LeftPaddle.move_and_collide(Vector2(0, -paddle_speed * delta))
+
+	if (Input.is_action_pressed("ui_down_02")):
+		$RightPaddle.move_and_collide(Vector2(0, paddle_speed * delta))
+	elif (Input.is_action_pressed("ui_up_02")):
+		$RightPaddle.move_and_collide(Vector2(0, -paddle_speed * delta))
