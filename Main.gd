00d9ace09f6ee85801b23a0315a2b48e3e1bdf33
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-01 11:36:43 -0400

    refactor: create paddles programmatically

--- Main.gd
+++ Main.gd
@@ -3,20 +3,35 @@ extends Node2D
 export var paddle_size: Vector2 = Vector2(6, 20)
 export var paddle_speed: int = 800
 
+var _paddle_offset = Vector2(10, 30)
+var _paddle_resource: Resource = load("res://Paddle.tscn")
+var _paddle1: KinematicBody2D = null
+var _paddle2: KinematicBody2D = null
+var _screen_size = Vector2(1024, 600)
+
 func _ready():
-	$Paddle1.scale = paddle_size
-	$Paddle2.scale = paddle_size
+	_create_paddles()
 
 func _process(delta):
 	if (Input.is_action_pressed("ui_cancel")):
 		get_tree().quit()
 
 	if (Input.is_action_pressed("ui_down_01")):
-		$Paddle1.move_and_collide(Vector2(0, paddle_speed * delta))
+		_paddle1.move_and_collide(Vector2(0, paddle_speed * delta))
 	elif (Input.is_action_pressed("ui_up_01")):
-		$Paddle1.move_and_collide(Vector2(0, -paddle_speed * delta))
+		_paddle1.move_and_collide(Vector2(0, -paddle_speed * delta))
 
 	if (Input.is_action_pressed("ui_down_02")):
-		$Paddle2.move_and_collide(Vector2(0, paddle_speed * delta))
+		_paddle2.move_and_collide(Vector2(0, paddle_speed * delta))
 	elif (Input.is_action_pressed("ui_up_02")):
-		$Paddle2.move_and_collide(Vector2(0, -paddle_speed * delta))
+		_paddle2.move_and_collide(Vector2(0, -paddle_speed * delta))
+
+func _create_paddles():
+	_paddle1 = _paddle_resource.instance()
+	_paddle1.scale = paddle_size
+	_paddle1.position = _paddle_offset
+	add_child(_paddle1)
+	_paddle2 = _paddle_resource.instance()
+	_paddle2.scale = paddle_size
+	_paddle2.position = _screen_size - _paddle_offset - paddle_size
+	add_child(_paddle2)
