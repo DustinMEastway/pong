5ccfe3c28b3e723a989e09d167e1a30fe1d19310
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-05 23:22:23 -0400

    CS50 the FPS update (55:53): add FPS label

--- Main.gd
+++ Main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+export var display_fps := false
+
 var _ball: Ball = load("res://Ball.tscn").instance()
+var _fps_label: Label
 var _paddle_offset = Vector2(10, 30)
 var _paddle_resource: Resource = load("res://Paddle.tscn")
 var _paddle1: Paddle = _paddle_resource.instance()
@@ -21,9 +24,13 @@ func _ready() -> void:
 	)
 	add_child(_paddle2)
 
+	if (display_fps):
+		_fps_label = Label.new()
+		add_child(_fps_label)
+
 	_reset_game()
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	if (Input.is_action_pressed("ui_cancel")):
 		get_tree().quit()
 	elif (Input.is_action_just_released("ui_accept")):
@@ -31,6 +38,10 @@ func _physics_process(delta) -> void:
 			GameService.state = "play"
 		else:
 			_reset_game()
+
+func _process(_delta):
+	if (display_fps):
+		_fps_label.text = String(Engine.get_frames_per_second()) + ' FPS'
 
 func _reset_game() -> void:
 	GameService.state = 'start'
