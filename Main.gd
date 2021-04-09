0d0a6799b837ca597aba504fff69c5b9913ed739
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-08 21:42:45 -0400

    CS50 the victory update (1:21:21): add victory after 10 points

--- Main.gd
+++ Main.gd
@@ -35,42 +35,50 @@ func _ready() -> void:
 	_reset_game()
 
 func _physics_process(_delta) -> void:
+	var accept_press = Input.is_action_just_released("ui_accept")
+	var player1_press = (
+		Input.is_action_just_pressed("ui_down_01")
+		or Input.is_action_just_pressed("ui_up_01")
+	)
+	var player2_press = (
+		Input.is_action_just_pressed("ui_down_02")
+		or Input.is_action_just_pressed("ui_up_02")
+	)
+
+
 	if (Input.is_action_just_pressed("ui_cancel")):
-		if (GameService.state == "serve"):
+		if (GameService.state == "serve" or GameService.state == "done"):
 			get_tree().quit()
 		else:
 			_reset_game()
-	elif (GameService.state == "serve" and _is_serve_pressed()):
+	elif (
+		GameService.state == "serve"
+		and (
+			accept_press
+			or (_serving_player == 1 and player1_press)
+			or (_serving_player == 2 and player2_press)
+		)
+	):
 		GameService.state = "play"
 		_update_labels()
+	elif (
+		GameService.state == "done"
+		and (accept_press or player1_press or player2_press)
+	):
+		_reset_game()
 
 func _process(_delta):
 	if (display_fps):
 		_fps_label.text = String(Engine.get_frames_per_second()) + ' FPS'
+
+	if (GameService.state != 'play'):
+		return
 
 	if (_ball.position.x < 0):
 		_update_score(1, _score1, _score2 + 1)
 	elif (_ball.position.x > GameService.screen_size.x):
 		_update_score(2, _score1 + 1, _score2)
 
-func _is_serve_pressed() -> bool:
-	return (
-		Input.is_action_just_released("ui_accept")
-		or (
-			_serving_player == 1
-			and (
-				Input.is_action_just_pressed("ui_down_01")
-				or Input.is_action_just_pressed("ui_up_01")
-			)
-		)
-		or (
-			_serving_player == 2
-			and (
-				Input.is_action_just_pressed("ui_down_02")
-				or Input.is_action_just_pressed("ui_up_02")
-			)
-		)
-	)
 func _reset_game() -> void:
 	_update_score(1, 0, 0)
 	_paddle1.reset()
@@ -85,10 +93,18 @@ func _update_score(serving_player: int, score1: int, score2: int):
 	_score1 = score1
 	_score2 = score2
 	_serving_player = serving_player
-	_ball.reset(serving_player)
 	$Score1.text = String(_score1)
 	$Score2.text = String(_score2)
-	_update_labels(
-		"Player " + String(serving_player) + "'s Serve",
-		"Press ENTER to serve!"
-	)
+	if (score1 < 10 and score2 < 10):
+		_ball.reset(serving_player)
+		_update_labels(
+			"Player " + String(serving_player) + "'s Serve!",
+			"Press ENTER to serve!"
+		)
+	else:
+		GameService.state = "done"
+		var winning_player = 1 if _serving_player == 2 else 2
+		_update_labels(
+			"Player " + String(winning_player) + " Won!",
+			"Press ENTER to play again!"
+		)
